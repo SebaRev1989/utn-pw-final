78a5e7de3f76fae6f119ecfd852bd2fb14b80ca8
Sebastián Reverso <sheba1989@gmail.com>
2016-07-06 09:50:28 -0300

    Configuracion del server

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Configuracion de dependencias para el server*/
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,21 +6,39 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/* Configuracion de dependencias para la db */
+
+var connection  = require('express-myconnection');
+var mysql = require('mysql');
+
 var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+// Configuracion del motor de render
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/* Conexion a la db */
+app.use(
+    connection(mysql,{
+        host: 'localhost',
+        user: 'root',
+        password : 'root',
+        port : 3306,
+        database:'progweb'
+    },'pool')
+);
+
+
+/* Configuracion de rutas */
 
 app.use('/', routes);
 
