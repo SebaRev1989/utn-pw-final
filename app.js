7b653f804b596c870c762896c5f91cbce04252a8
Sebastián Reverso <sheba1989@gmail.com>
2016-07-07 09:28:35 -0300

    Vista contenedora

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 /* Configuracion de dependencias para la db */
 
@@ -18,6 +19,8 @@ var app = express();
 // Configuracion del motor de render
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
