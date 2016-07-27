cfa7c3f95f13120891504ebb401254b91f6e6a7c
Sebastián Reverso <sheba1989@gmail.com>
2016-07-27 10:59:49 -0300

    Visualizacion de las historias clinicas

--- controllers/ch_controller.js
+++ controllers/ch_controller.js
@@ -1,11 +1,27 @@
 exports.index = function(req, res) {
 	var pacId = req.params.pacId;
+	var consulta = 'SELECT histClinica.fecha AS fecha, histClinica.descripcion AS descripcion, paciente.apellido '
+		+ 'AS pacApellido, paciente.nombre AS pacNombre, medico.apellido AS medApellido, medico.nombre AS medNombre, '
+		+ 'paciente.pacId AS pacId FROM histClinica INNER JOIN paciente ON histClinica.pacId = paciente.pacId '
+		+ 'INNER JOIN medico ON histClinica.medId = medico.medId WHERE paciente.pacId = ? ORDER BY histClinica.fecha ASC';
 	req.getConnection(function(err, connection) {
-		connection.query('SELECT * FROM paciente WHERE pacId = ? ', [pacId], function(err, rows) {
+		connection.query(consulta, [pacId], function(err, rows) {
 			if (err) {
-				console.log("Error al buscar paciente: %s", err);
+				console.log("Error al buscar historia clinica: %s", err);
 			} else {
-				res.render('ch/index.ejs', {pac : rows});
+				if (rows.length === 0) {
+					var consulta = 'SELECT pacId AS pacId, apellido AS pacApellido, nombre AS pacNombre '
+						+ 'FROM paciente WHERE pacId = ?';
+					connection.query(consulta, [pacId], function(err, rows) {
+						if (err) {
+							console.log("Error al buscar paciente: %s", err);
+						} else {
+							res.render('ch/index.ejs', {data : rows});
+						}
+					});
+				} else {
+					res.render('ch/index.ejs', {data : rows});
+				}
 			}
 		});
 	});
